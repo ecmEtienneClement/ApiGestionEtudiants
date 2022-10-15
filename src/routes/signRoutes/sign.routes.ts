2c5deb5ad65b2d0fbb10f41a659b81337d7adbfe
ecmEtienneClement <86694611+ecmEtienneClement@users.noreply.github.com>
2022-10-15 17:58:57 +0000

    after delete src

--- src/routes/signRoutes/sign.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import signCtrl from "./sign.ctrl";
-const router = Router();
-
-//MISE EN PLACE DES METHODES
-router.post("/in", signCtrl.signIn);
-
-//TODO
-export default router;
